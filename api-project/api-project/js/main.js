46dfb3d58c19ea490f095848828362487afdc901
TinaGu0 <tinaguo0904@gmail.com>
2023-12-04 10:15:14 -0500

    aaaaaaaaaaaa

--- api-project/api-project/js/main.js
+++ api-project/api-project/js/main.js
@@ -1,3 +1,27 @@
 import "../css/style.css"
 
+
+const URL = `https://api.quotable.io/random`;
+
+async function getData(URL){
+    try {
+        //requesting a response REST API's 
+        const response = await fetch(URL); 
+        if(response.status != 200) {
+            throw new Error(response.statusText);
+        }
+        console.log(error, ":(");
+        //convert response to json 
+        const data = await response.json()
+        console.log(response); 
+        document.querySelector("h1").textContent = data.content;
+    } catch (error) {
+        
+    }
+}
+ getData(URL); 
+
+
+
+
 console.log("hi")
