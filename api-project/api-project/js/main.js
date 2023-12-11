0f759efc8acc52e1ffc3e431c098331f4d7d4a40
TinaGu0 <tinaguo0904@gmail.com>
2023-12-11 10:13:59 -0500

    asdfghfsda

--- api-project/api-project/js/main.js
+++ api-project/api-project/js/main.js
@@ -7,23 +7,37 @@ import "./niji"
 const DOMSelectors = {
     profiles: document.querySelector(".profiles")
 }
- 
-function cards(arr) {
-    const data = require("./niji")
-    arr.forEach((allLivers) => DOMSelectors.profiles.insertAdjacentHTML("beforeend", 
-`
-    <div class="profile">
-        <h2>${allLivers.name}</h2>    
-        <h3>${allLivers.enName}</h3>
-        <h3>${allLivers.vision}</h3>
-        <img src="${allLivers.head}" />
-        <h3>${allLivers.subscriberCount}</h3>
-    </div>
-`
-))};
 
-const arrau = [ 1, 2, 3, 4, 5]
-cards(arrau);
+
+const URL = `https://corsproxy.io/?https://www.nijisanji.jp/_next/data/qn3MdhaKNyN63H2JwZiWb/ja/talents.json`;
+
+async function getData(URL){
+    try {
+        const response = await fetch(URL); 
+        const data = await response.json();
+        console.log(data); 
+        data.pageProps.allLivers.forEach((livers) => { console.log(livers)});
+
+        function cards(arr) {
+            arr.forEach((data) => DOMSelectors.profiles.insertAdjacentHTML("beforeend", 
+        `
+            <div class="profile">
+                <h2>${data.pageProps.allLivers.name}</h2>    
+                <h3>${data.pageProps.allLivers.enName}</h3>
+                <h3>${data.pageProps.allLivers.name}</h3>
+    /*           <img src="${data.pageProps.allLivers.images.head}" /> */
+                <h3>${data.pageProps.allLivers.subscriberCount}</h3>
+            </div>
+        `
+        ))};
+        cards(ee);
+
+    } catch (error) {
+        
+    }
+};
+
+ getData(URL); 
 
 
 
--- api-project/api-project/js/niji.js
+++ api-project/api-project/js/niji.js
@@ -1,4 +1,4 @@
-
+/* 
 const URL = `https://corsproxy.io/?https://www.nijisanji.jp/_next/data/qn3MdhaKNyN63H2JwZiWb/ja/talents.json`;
 
 async function getData(URL){
@@ -8,10 +8,9 @@ async function getData(URL){
         console.log(data); 
         data.pageProps.allLivers.forEach((livers) => { console.log(livers) 
         });
-        module.exports = {data};
     } catch (error) {
         
     }
 }
  getData(URL); 
-
+ */
